2859306564bf93b706c45412525d6166205920b0
IanniMuliterno <iannimuliterno@gmail.com>
2021-05-03 19:21:31 -0300

    rascunho do cv

---
+++ script_inicial.R
@@ -0,0 +1,10 @@
+usethis::use_git()
+usethis::use_github()
+
+
+library(pagedown)
+install.packages("devtools")
+devtools::install_github("mitchelloharawild/icons")
+devtools::install_github("IanniMuliterno/datadrivencv")
+
+# depois disso inicie um novo rmarkdown com o template de cv da pagedown
