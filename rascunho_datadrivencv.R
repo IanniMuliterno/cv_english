1d32ece7411e214f172566a42fd3bdb67cca0beb
IanniMuliterno <iannimuliterno@gmail.com>
2021-05-16 10:28:20 -0300

    cv em portugues

---
+++ rascunho_datadrivencv.R
@@ -0,0 +1,20 @@
+# testando datadriven
+
+# run ?datadrivencv::use_datadriven_cv to see more details
+# datadrivencv::use_datadriven_cv(
+#   full_name = "Ianní Muliterno",
+#   data_location = "https://docs.google.com/spreadsheets/d/14MQICF2F8-vf8CKPF1m4lyGKO6_thG-4aSwat1e2TWc",
+#   pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
+#   html_location = "nickstrayer.me/cv/",
+#   source_location = "https://github.com/nstrayer/cv"
+# )
+
+datadrivencv::use_datadriven_cv(
+  full_name = "Ianní Muliterno",
+  data_location = "https://docs.google.com/spreadsheets/d/14MQICF2F8-vf8CKPF1m4lyGKO6_thG-4aSwat1e2TWc",
+  pdf_location = "https://github.com/IanniMuliterno/cv_english/raw/master/ianni_cv.pdf",
+  #html_location = "nickstrayer.me/cv/",
+  source_location = "https://github.com/IanniMuliterno/cv_english"
+)
+
+
